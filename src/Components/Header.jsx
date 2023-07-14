3ac0b6cb15f7620248c626fea8c0ae255164f8c6
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-07-14 19:45:40 -0300

    fix: general styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -24,16 +24,16 @@ function Header({ onFavoritesClick}) {
 
   return (
     <header className='flex justify-between border-b-2 p-2'>
-      <div className='w-1/12 p-2'>
+      <div className='w-1/12 p-1'>
         <img src="src\assets\Marvel_Logo.svg.png" alt="Marvel Logo" />
       </div>
 
       <form className='flex items-center justify-stretch w-10/12'>
-        <input type="text" value={searchValue} onChange={handleInputChange} placeholder="Search character" className='border-l-2 w-full p-2 focus:outline-none'/>
+        <input type="text" value={searchValue} onChange={handleInputChange} placeholder="Search character" className='border-l-2 w-full p-1 focus:outline-none'/>
       </form>
 
       <button onClick={onFavoritesClick} className='w-1/12'>
-        <StarIcon className='w-10 m-auto opacity-50'/>
+        <StarIcon className='w-7 m-auto opacity-50'/>
       </button>
     </header>
   );
