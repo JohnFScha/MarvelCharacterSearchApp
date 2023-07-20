c81de754bd1dc181cbc5f82694d18d3c1c370a51
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-07-20 19:45:42 -0300

    feature: add character list

---
+++ src/Components/CharacterList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CharacterList = ({ characters }) => {
+  return (
+    <ul>
+      {characters.map((character) => (
+        <li key={character.id}>
+          <img src={character.image} alt={character.name} onClick={() => onCharacterClick(character)} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default CharacterList
