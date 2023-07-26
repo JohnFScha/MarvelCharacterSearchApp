a9392b0f766497976383878cd6663425192fd065
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-07-26 15:50:54 -0300

    add: .env file

--- src/Helpers/marvelApi.js
+++ src/Helpers/marvelApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://gateway.marvel.com/v1/public";
-const API_KEY = ENV_KEY;
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY = process.env.API_KEY;
 
 export const getCharactersAPI = async () => {
   try {
